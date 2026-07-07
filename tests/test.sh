e2463c4c9741216517f61df4ce582d3af7436975
amsalman1 <hadisalmanx@gmail.com>
2026-07-07 01:02:30 -0400

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /logs/verifier
+
+pytest /tests/test_outputs.py -q --ctrf /logs/verifier/ctrf.json
+status=$?
+
+if [ "$status" -eq 0 ]; then
+  echo 1 > /logs/verifier/reward.txt
+else
+  echo 0 > /logs/verifier/reward.txt
+fi
+
+exit 0
